165f366522594ea0f4de6ee72fba5a302029fcc4
xelawe <xelawe@users.noreply.github.com>
2016-10-31 20:48:57 +0100

    introduced time_tool

--- SonoffRoomFountain.ino
+++ SonoffRoomFountain.ino
@@ -1,5 +1,5 @@
 /*
-Idea: https://tzapu.com/sonoff-firmware-boilerplate-tutorial/
+  Idea: https://tzapu.com/sonoff-firmware-boilerplate-tutorial/
 
     Flash Mode: DIO
     Flash Frequency: 40MHz
@@ -29,6 +29,15 @@ Idea: https://tzapu.com/sonoff-firmware-boilerplate-tutorial/
 
 */
 
+#define serdebug
+#ifdef serdebug
+#define DebugPrint(...) {  Serial.print(__VA_ARGS__); }
+#define DebugPrintln(...) {  Serial.println(__VA_ARGS__); }
+#else
+#define DebugPrint(...) { }
+#define DebugPrintln(...) { }
+#endif
+
 #define SONOFF_BUTTON    0
 #define SONOFF_RELAY    12
 #define SONOFF_LED      13
@@ -47,10 +56,10 @@ static bool BLYNK_ENABLED = true;
 #include <DNSServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
+#include "time_tool.h"
+
 #include <EEPROM.h>
 
-#include <WiFiUdp.h>
-#include <TimeAlarms.h>
 
 #define EEPROM_SALT 12663
 typedef struct {
@@ -84,20 +93,6 @@ int InputState = HIGH;
 
 static long startPress = 0;
 
-/* Don't hardwire the IP address or we won't get the benefits of the pool.
-    Lookup the IP address for the host name instead */
-IPAddress timeServerIP; // time.nist.gov NTP server address
-const char* ntpServerName = "time.nist.gov";
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP Udp;
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-const int timeZone = 2;     // Central European Time
-
-
 void tick()
 {
   //toggle state
@@ -115,8 +110,6 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   ticker.attach(0.2, tick);
 }
 
-
-
 void setState(int s) {
 
   int lv_s = s;
@@ -126,7 +119,6 @@ void setState(int s) {
     lv_s = relStateOFF;
     relayState = lv_s;
   }
-
 
   Serial.println(relayState);
 
@@ -240,6 +232,8 @@ BLYNK_READ(5) {
 void setup()
 {
   Serial.begin(115200);
+
+  DebugPrintln("\n" + String(__DATE__) + ", " + String(__TIME__) + " " + String(__FILE__));
 
   //set led pin as output
   pinMode(SONOFF_LED, OUTPUT);
@@ -355,21 +349,7 @@ void setup()
   InputState = digitalRead(SONOFF_INPUT);
   attachInterrupt(SONOFF_INPUT, toggleInput, CHANGE);
 
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-
-  Serial.println("waiting for sync");
-  //get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP);
-
-  setSyncProvider(getNtpTime);
-  while (timeStatus() == timeNotSet) { // wait until the time is set by the sync provider
-    setSyncInterval(1); // Try to sync each second
-    delay(2000);
-  }
-  setSyncInterval(300); // Sync every 5 minutes
+  init_time();
 
   turnOff();
 
@@ -391,9 +371,6 @@ void loop()
   if (BLYNK_ENABLED) {
     Blynk.run();
   }
-
-  //delay(200);
-  //Serial.println(digitalRead(SONOFF_BUTTON));
 
   switch (cmd_inp) {
     case CMD_WAIT:
@@ -437,60 +414,7 @@ void loop()
       break;
   }
 
+  check_time();
+  
   Alarm.delay( 0 );
-
 }
-
-
-
-/*-------- NTP code ----------*/
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  //sendNTPpacket(timeServer);
-  sendNTPpacket(timeServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress & address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
-
-
-
